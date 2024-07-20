aa5b4a9a8c8558d4ea0ac6daa0f30adcec6bcc8d
echat <echat@ETWEN>
2024-07-21 00:32:03 +0200

    Ajoutez des fichiers projet.

---
+++ WinFormsApp1/Form1.Designer.vb
@@ -0,0 +1,155 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Button1 = New Button()
+        Button2 = New Button()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        Timer1 = New Timer(components)
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(3, 339)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(75, 23)
+        Button1.TabIndex = 0
+        Button1.Text = "DDOS"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(84, 339)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 23)
+        Button2.TabIndex = 1
+        Button2.Text = "STOP!!!"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(58, 60)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(17, 15)
+        Label1.TabIndex = 2
+        Label1.Text = "IP"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(207, 60)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(35, 15)
+        Label2.TabIndex = 3
+        Label2.Text = "PORT"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(375, 60)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(27, 15)
+        Label3.TabIndex = 4
+        Label3.Text = "SEC"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(176, 343)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(71, 15)
+        Label4.TabIndex = 5
+        Label4.Text = "MAX SEC 50"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(25, 90)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(100, 23)
+        TextBox1.TabIndex = 6
+        TextBox1.Text = "EX 0.0.0.0"
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(176, 90)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(100, 23)
+        TextBox2.TabIndex = 7
+        TextBox2.Text = "80"
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(341, 90)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(100, 23)
+        TextBox3.TabIndex = 8
+        TextBox3.Text = "50"
+        ' 
+        ' Timer1
+        ' 
+        Timer1.Interval = 5000
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), Image)
+        BackgroundImageLayout = ImageLayout.Stretch
+        ClientSize = New Size(463, 365)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        DoubleBuffered = True
+        Name = "Form1"
+        Text = "BOOTER TEAM CODE NIGHT BY FIZZERYTB"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Timer1 As Timer
+
+End Class
---
+++ WinFormsApp1/Form1.vb
@@ -0,0 +1,37 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+
+        Timer1.Stop()
+        Timer1.Interval = TextBox2.Text
+        Timer1.Start()
+        TextBox1.Enabled = False
+        TextBox2.Enabled = False
+        Button1.Enabled = False
+        Button2.Enabled = True
+
+    End Sub
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+
+        Timer1.Stop()
+        TextBox1.Enabled = True
+        TextBox2.Enabled = True
+        Button1.Enabled = True
+        Button2.Enabled = False
+
+    End Sub
+
+    Private Sub Timer.Tick(sender As System.Object, e As System.EventArgs) Handles Timer1.Tick
+
+        Try
+
+            My.Compteur.Network.Ping(TextBox1.Text)
+
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+End Class
